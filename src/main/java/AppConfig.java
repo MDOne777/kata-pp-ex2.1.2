50be3e217a91438212fff38ed5fd10f9f04ba2c7
MDOne <llapotkovs@yandex.ru>
2024-11-02 08:59:42 +0300

    Commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Random;
+
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope(scopeName = "prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("Meow-meow " + new Random().nextInt());
+        return cat;
     }
 }
